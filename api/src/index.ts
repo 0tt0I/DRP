3da1a3b3b556e8d7eac63c010f8c35293a16704b
0tt0I <ow20@ic.ac.uk>
2022-06-09 18:42:41 +0100

    feat: use CORS middleware for backend request handling

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,9 +1,14 @@
 import express from 'express'
+import cors from 'cors'
 
 // TODO: Replace with environment config
 const port = 3000
 
 const app = express()
+app.use(cors())
 
-app.get('/api', (_req, res) => res.sendStatus(200))
+app.get('/api', (_req, res) => {
+  console.log('Received request. ')
+  res.status(200).json({ name: 'Otto' })
+})
 app.listen(port, () => console.log(`Listening on port ${port}`))
