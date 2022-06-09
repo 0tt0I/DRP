498a97d176d973038e58f3482305057e68b33339
r-ushil <rushil.patel791@gmail.com>
2022-06-09 11:58:10 +0100

    feat: added react form hooks to current form for validation and grabbing information

--- frontend/pages/login.tsx
+++ frontend/pages/login.tsx
@@ -1,26 +1,52 @@
 import Head from "next/head"
 import { useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+// typescript interface for form input
+interface Inputs {
+    email: string
+    password: string
+}
 
 function Login() {
 
+    //react state to check whether signing up or not
     const [login, setLogin] = useState(false);
 
+    //form hooks
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+      } = useForm<Inputs>()
+    
+    const onSubmit: SubmitHandler<Inputs> = async (data) => { console.log(data) }
+
+    //HTML elements of the page
+    //handleSubmit validates input, register gets data
+
     return (
-        <div>
+        <div className="relative flex h-screen w-screen flex-col bg-black md:items-center md:justify-center md:bg-transparent">
             <Head>
                 <title> Login Page</title>
                 <link rel="icon" href="coffee-icon.png" />
             </Head>
 
-
-            <form>
+            <form onSubmit={handleSubmit(onSubmit)}>
                 <h1>Sign In Page</h1>
                 <div>
                     <label>
-                        <input type="email" placeholder="Email" className="input" />
+                        <input type="email" 
+                        placeholder="Email" 
+                        className="input" 
+                        {...register("email", {required: true})}/>
                     </label>
                     <label>
-                        <input type="password" placeholder="Password" className="input" />
+                        <input type="password"
+                        placeholder="Password"
+                        className="input"
+                        {...register("password", {required: true})}/>
                     </label>
                 </div>
 
