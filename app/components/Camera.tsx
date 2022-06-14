78ae1d257225c262817801af66b3334263319c46
r-ushil <rushil.patel791@gmail.com>
2022-06-14 01:44:34 +0100

    feat: added ability to capture webcam picture

--- app/components/Camera.tsx
+++ app/components/Camera.tsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useCallback, useRef, useState } from "react";
 import Webcam from "react-webcam";
 
 
@@ -14,9 +14,23 @@ const videoConstraints = {
 function Camera() {
 
     //reference to webcam
-    const cameraRef = useRef(null);
+    const cameraRef = useRef<Webcam>(null);
+    //state for captured image
+    const [image, setImage] = useState<string | null>(null);
 
-    //set webcam parameters
+    //capture function with react hook (to improve performance)
+    const capture = useCallback(() => {
+
+        const currentRef = cameraRef.current;
+
+        if (currentRef) {
+            const imageSrc = currentRef.getScreenshot();
+            setImage(imageSrc);
+        }
+
+    }, [cameraRef])
+
+    //set webcam parameters, and run capture function and display
     return ( 
         <div className="camera">
             <Webcam
@@ -27,6 +41,13 @@ function Camera() {
                 width = {videoConstraints.width}
                 videoConstraints = {videoConstraints}
             />
+            <button 
+            className="cameraCapture_button"
+            onClick={capture}
+            > TAKE PICTURE </button>
+            {image && (
+                <img src={image} />
+            )}
         </div>
     )
 }
