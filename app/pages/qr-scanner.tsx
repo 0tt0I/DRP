165b20672204b3851fac3e57b19b5d25c7037f2f
Abdullah, Fawwaz <crashmacompilers@gmail.com>
2022-06-15 14:51:10 +0100

    fix: Removed broken dependency on react-qr-reader and used zxing modules directly

--- app/pages/qr-scanner.tsx
+++ app/pages/qr-scanner.tsx
@@ -1,61 +1,74 @@
-import RenderResult from 'next/dist/server/render-result';
-import React, { useEffect, useState } from 'react';
-import { OnResultFunction, QrReader } from 'react-qr-reader';
-import { auth } from '../firebase';
-import { checkNewCustomer } from '../services/businessQrScan';
-
+import React, { useEffect, useState } from 'react'
+import { BrowserCodeReader, BrowserQRCodeReader } from '@zxing/browser'
+import { auth } from '../firebase'
+import { checkNewCustomer } from '../services/businessQrScan'
 
 export default function QRScanner () {
-  const [data, setData] = useState("no result")
-  const [qrString, setQrString] = useState("")
+  // QR code reader.
+  const reader = new BrowserQRCodeReader()
 
-    const handleResult: OnResultFunction = async (result, error) => {
-        if (result) {
-            setQrString(result.getText())
-        }
+  // Result box.
+  const [data, setData] = useState('no result')
+  const [decodeResult, setDecodeResult] = useState('')
 
-        if (error) {
-            console.info(error);
-        }
+  // Video devices.
+  const [selectedDevice, setSelectedDevice] = useState('')
+
+  // Initialise video devices.
+  useEffect(() => {
+    const getVideos = async () =>
+      BrowserCodeReader.listVideoInputDevices()
+        .then(
+          devices => setSelectedDevice(devices[0].deviceId)
+        )
+        .catch(err =>
+          console.info(err)
+        )
+
+    getVideos()
+  }, [])
+
+  // Now actually read.
+  useEffect(() => {
+    reader.decodeOnceFromVideoDevice(selectedDevice, 'scanner-preview')
+      .then(result => setDecodeResult(result.getText()))
+      .catch(console.info)
+  }, [selectedDevice])
+
+  // Run something...
+  useEffect(() => {
+    async function updateData() {
+      const newCust = (await checkNewCustomer(
+        decodeResult,
+        (auth.currentUser) ? auth.currentUser!.uid : '')).newUser
+
+      switch (newCust) {
+      case -1:
+        setData('Invalid QR-code')
+        break
+      case 0:
+        setData('This customer has had a discount before...')
+        break
+      case 1:
+        setData('This is a new customer! Treat them to a discount')
+        break
+      case 2:
+        setData('This referral is for a different business.')
+        break
+      default:
+        break
+      }
     }
-
-    useEffect(() => {
-        async function updateData() {
-            const newCust = (await checkNewCustomer(
-                qrString,
-                (auth.currentUser) ? auth.currentUser!.uid : '')).newUser
-
-            switch (newCust) {
-                case -1:
-                    setData("Invalid QR-code")
-                    break;
-                case 0:
-                    setData("This customer has had a discount before...")
-                    break;
-                case 1:
-                    setData("This is a new customer! Treat them to a discount")
-                    break;
-                case 2:
-                    setData("This referral is for a different business.")
-                default:
-                    break;
-            }
-        }
-
-        console.log("effect used")
-        updateData()
-
-    }, [qrString])
-
+    updateData()
+  }, [decodeResult])
 
   return (
     <div>
-      <QrReader
-        onResult={handleResult}
-        videoStyle={{ width: "100%" }}
-        constraints={{ facingMode: 'user' }}
-      />
-      <p>{data}</p>
+      <video id='scanner-preview' width='512' height='512' />
+      <br />
+      {decodeResult}
+      <br />
+      {data}
     </div>
-  );
+  )
 }
