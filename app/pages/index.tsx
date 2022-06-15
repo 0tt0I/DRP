c3f5f12c47123fc452009952096f2b955c290b29
Abdullah, Fawwaz <crashmacompilers@gmail.com>
2022-06-15 15:34:30 +0100

    style: styled the business home and scanner page

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -11,13 +11,13 @@ export default function Home () {
   }
 
   return (
-    <div className="relative grid grid-cols-1 grid-rows-1 h-screen w-screen items-center justify-center">
-      <div className="place-self-center flex flex-col gap-4 bg-violet-300 p-4 rounded-lg">
-        <h1 className="text-3xl text-violet-900 font-bold">
+    <div className="home-div">
+      <div className="home-subdiv">
+        <h1>
           What would you like to do?
         </h1>
 
-        <div className="flex flex-row gap-4 place-content-center">
+        <div className="home-buttondiv">
           <button onClick={logout} className="general-button">
             Log Out
           </button>
