21ad06221e9f94f85ab678ffe9683c7174e8e40d
r-ushil <rushil.patel791@gmail.com>
2022-06-16 20:55:05 +0000

    feat: displayed docs from firestore on page using useEffect hook

--- app/pages/edit-discounts.tsx
+++ app/pages/edit-discounts.tsx
@@ -1,9 +1,11 @@
 import { Dialog } from '@headlessui/react'
-import { addDoc, collection, doc } from 'firebase/firestore'
+import { addDoc, collection, CollectionReference, getDocs } from 'firebase/firestore'
 import { useRouter } from 'next/router'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import HomeButton from '../components/HomeButton'
 import { auth, db } from '../firebase'
+import { Discount } from '../types/FirestoreCollections'
+import { createHash } from 'crypto'
 
 export default function SetDiscounts () {
   const router = useRouter()
@@ -11,12 +13,29 @@ export default function SetDiscounts () {
   // modal state for popup and info for qr-scan
   const [inputOpen, setInputOpen] = useState(false)
 
-  // state for description input
-  // eslint-disable-next-line no-unused-vars
+  // state for description inpu
   const [newDescription, setNewDescription] = useState('')
   // state for points input
-  // eslint-disable-next-line no-unused-vars
-  const [newPoints, setNewPoints] = useState('')
+  const [newPoints, setNewPoints] = useState(0)
+  // state for discount collection ref
+
+  const businessUid = auth.currentUser!.uid
+  const discountCollection = collection(db, 'businesses', businessUid, 'discounts') as CollectionReference<Discount>
+
+  // set state for referrals
+  const [discounts, setDiscounts] = useState<Discount[]>([])
+
+  useEffect(() => {
+    const getDiscounts = async () => {
+      const discountCollection = collection(db, 'businesses', businessUid, 'discounts') as CollectionReference<Discount>
+
+      const data = await getDocs(discountCollection)
+      // get relevant information from document
+      setDiscounts(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
+    }
+
+    getDiscounts()
+  })
 
   const createDiscount = async () => {
     // business should already be logged in
@@ -24,21 +43,18 @@ export default function SetDiscounts () {
 
     console.log(businessUid)
 
-    // null check
-    if (businessUid) {
-      const discountCollection = collection(db, 'businesses', businessUid, 'discounts')
-      const newDiscount = { description: newDescription, points: newPoints }
+    const newDiscount = { description: newDescription, points: newPoints }
 
-      await addDoc(discountCollection, newDiscount)
-    }
+    const data = await getDocs(discountCollection)
+    // get relevant information from document
+    setDiscounts(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
+
+    await addDoc(discountCollection, newDiscount)
   }
 
   return (
     <div className="home-div">
       <div className="home-subdiv">
-        <h1>
-              Set Discounts
-        </h1>
         <Dialog open={inputOpen} onClose={() => setInputOpen(false)} className="relative z-50">
           <div className="fixed inset-0 flex items-center justify-center p-4 drop-shadow-lg">
             <Dialog.Panel className="w-full max-w-md overflow-hidden p-4 text-left align-middle shadow-xl transition-all flex flex-col gap-4 ultralight-div">
@@ -64,7 +80,7 @@ export default function SetDiscounts () {
                     className="input"
                     type="tel"
                     onChange={(event) => {
-                      setNewPoints(event.target.value)
+                      setNewPoints(Number(event.target.value))
                     }}/>
                 </label>
               </div>
@@ -83,10 +99,36 @@ export default function SetDiscounts () {
           </div>
         </Dialog>
 
-        <button className="general-button" onClick={() => setInputOpen(true)}>  Add </button>
+        <div className="flex flex-col gap-4 p-4 lighter-div">
+          <h2 className="font-bold text-center text-4xl text-dark-nonblack">Set Discounts</h2>
 
-        <HomeButton router={router} where="/business-home" />
+          {discounts.length > 0
+            ? discounts.map(DiscountEntry)
+            : <p className="text-warning text-2xl p-8">There are no active discounts.</p>}
+
+          <button className="general-button" onClick={() => setInputOpen(true)}>  Add </button>
+          <HomeButton router={router} where="/business-home" />
+        </div>
       </div>
     </div>
   )
+
+  function DiscountEntry (ref: Discount) {
+    return (
+      <div
+        key={createHash('sha256').update(JSON.stringify(ref)).digest('hex').toString()}
+        className="flex flex-col gap-4 place-content-center p-4 default-div rounded-lg max-w-max">
+        <div className="flex flex-row gap-4 place-content-start">
+
+          <div className="ref-info grid grid-cols-3 grid-flow-row-dense place-content-center gap-2 w-fit">
+            <h1 className="font-bold text-dark-nonblack">Description: </h1>
+            <p className="col-span-2">{ref.description}</p>
+
+            <h1 className="font-bold text-dark-nonblack w-32">Points Worth: </h1>
+            <p className="col-span-2">{ref.points}</p>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
