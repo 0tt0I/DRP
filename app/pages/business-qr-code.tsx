c100a146bae43bf050f085fe92aa33454fea2805
r-ushil <rushil.patel791@gmail.com>
2022-06-16 12:54:01 +0000

    feat: added business uid qr code for businesses

--- app/pages/business-qr-code.tsx
+++ app/pages/business-qr-code.tsx
@@ -1,7 +1,17 @@
 import { useRouter } from 'next/router'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { BrowserQRCodeSvgWriter } from '@zxing/browser'
+import { auth } from '../firebase'
+
 export default function BusinessQRCode () {
   const router = useRouter()
+  const qrCodeWriter = new BrowserQRCodeSvgWriter()
+  const [qrImage, setQrImage] = useState<SVGSVGElement>()
+
+  useEffect(() => {
+    const businessUid = auth.currentUser!.uid
+    setQrImage(qrCodeWriter.write(businessUid, 150, 150))
+  }, [])
 
   return (
     <div className="home-div">
@@ -9,6 +19,11 @@ export default function BusinessQRCode () {
         <h1>
               Your Unique Business QR Code
         </h1>
+        <div className="place-self-center">
+          <svg
+            className="w-[150px] h-[150px] bg-left"
+            dangerouslySetInnerHTML={{ __html: qrImage ? qrImage.innerHTML : '' }}/>
+        </div>
 
         <div className="home-buttondiv">
 
