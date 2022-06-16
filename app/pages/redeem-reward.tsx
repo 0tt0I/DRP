4978653448e663b9d31cc2486425ff87c01ef48a
r-ushil <rushil.patel791@gmail.com>
2022-06-16 22:01:09 +0000

    feat: added customer qr code with their uid

--- app/pages/redeem-reward.tsx
+++ app/pages/redeem-reward.tsx
@@ -1,9 +1,21 @@
 import { useRouter } from 'next/router'
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
+import { BrowserQRCodeSvgWriter } from '@zxing/browser'
+import { auth } from '../firebase'
 import HomeButton from '../components/HomeButton'
 
 export default function RedeemReward () {
   const router = useRouter()
+  const qrCodeWriter = new BrowserQRCodeSvgWriter()
+  const svg = useRef<HTMLInputElement>(null)
+
+  useEffect(() => {
+    const userUid = auth.currentUser!.uid
+    console.log(userUid)
+    if (svg.current) {
+      svg.current.appendChild(qrCodeWriter.write(userUid, 256, 256))
+    }
+  }, [])
 
   return (
     <div className="home-div">
@@ -11,6 +23,9 @@ export default function RedeemReward () {
         <h1>
             Redeem Your Rewards
         </h1>
+        <div className="place-self-center white-div">
+          <div className="w-[256px] h-[256px] bg-left" ref={svg}/>
+        </div>
         <HomeButton router={router} where="/" />
       </div>
     </div>
