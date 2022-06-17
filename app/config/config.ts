26fb61e482564db813e0eaa6233cde8964da2eef
0tt0I <ow20@ic.ac.uk>
2022-06-17 21:11:17 +0100

    chore: split out firebase into environmentConfig using dotenv, separate files

---
+++ app/config/config.ts
@@ -0,0 +1,13 @@
+import { config } from 'dotenv'
+
+config()
+
+export const environmentConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSender: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+}
---
+++ app/plugins/firebase.ts
@@ -0,0 +1,29 @@
+import { initializeApp, getApp, getApps } from 'firebase/app'
+import { collection, CollectionReference, DocumentData, getFirestore } from 'firebase/firestore'
+import { getAuth } from 'firebase/auth'
+import { getStorage } from 'firebase/storage'
+import { environmentConfig } from '../config/config'
+
+const firebaseConfig = {
+  apiKey: environmentConfig.apiKey,
+  authDomain: environmentConfig.authDomain,
+  projectId: environmentConfig.projectId,
+  storageBucket: environmentConfig.storageBucket,
+  messagingSender: environmentConfig.messagingSender,
+  appId: environmentConfig.appId,
+  measurementId: environmentConfig.measurementId
+}
+
+// Initialize Firebase
+// Singleton pattern for getting firebase instance
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp()
+const db = getFirestore()
+const auth = getAuth()
+const storage = getStorage()
+
+export const createCollection = <T = DocumentData>(collectionName: string) => {
+  return collection(db, collectionName) as CollectionReference<T>
+}
+
+export default app
+export { db, auth, storage }
