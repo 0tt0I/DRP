d35e74fb0af0c741063ed8d3d804b87a50a75588
r-ushil <rushil.patel791@gmail.com>
2022-06-20 21:27:56 +0000

    feat: implemented getUserReferrals controller

--- api/src/controllers/customer.controllers.ts
+++ api/src/controllers/customer.controllers.ts
@@ -1,5 +1,6 @@
-import { doc, getDoc, updateDoc } from '@firebase/firestore'
+import { doc, getDoc, updateDoc, collection, CollectionReference, getDocs, query, where } from '@firebase/firestore'
 import { Request, Response } from 'express'
+import { Referral } from '../models/FirestoreCollections'
 import { db } from '../plugins/firebase'
 
 export async function customerGetPointsController (req: Request, res: Response) {
@@ -28,4 +29,16 @@ export async function customerUpdatePointsController (req: Request, res: Respons
   })
 
   res.status(200)
+}
+
+export async function customerGetUserReferrals (req: Request, res: Response) {
+  const customerUid: string = req.body.customerUid
+
+  const collectionsRef = collection(db, 'referrals') as CollectionReference<Referral>
+  const data = await getDocs(query(collectionsRef, where('customerUid', '==', customerUid)))
+
+  // get relevant information from document
+  const referrals = (data.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
+
+  res.status(200).json({ referrals })
 }
