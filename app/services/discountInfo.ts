61e157d2990932f63cdf7030c98be3e42684b081
r-ushil <rushil.patel791@gmail.com>
2022-06-20 13:06:31 +0000

    feat: changed getDiscountPoints to return description as well

--- app/services/discountInfo.ts
+++ app/services/discountInfo.ts
@@ -1,13 +1,13 @@
 import { doc, getDoc } from '@firebase/firestore'
 import { db } from '../firebase'
 
-export async function getDiscountPoints (businessUid: string, discountUid: string): Promise<number> {
+export async function getDiscountPointsAndDescription (businessUid: string, discountUid: string): Promise<[number, string]> {
   const docRef = doc(db, 'businesses', businessUid, 'discounts', discountUid)
   const docSnap = await getDoc(docRef)
 
   if (docSnap.exists()) {
-    return (docSnap.data().points)
+    return [(docSnap.data().points), docSnap.data().description]
   } else {
-    return -1 // document doesn't exist
+    return [-1, ''] // document doesn't exist
   }
 }
