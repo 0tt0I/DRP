e4a32ad4adfddf6ba14b26822dfddf7dd9039d16
r-ushil <rushil.patel791@gmail.com>
2022-06-20 21:32:41 +0100

    feat: added controller to router

--- api/src/routes/business.routes.ts
+++ api/src/routes/business.routes.ts
@@ -2,10 +2,12 @@ import { Router } from 'express'
 
 import {
   businessQRScanController,
-  businessAwardPointsController
+  businessAwardPointsController,
+  businessGetNameAndDiscount
 } from '../controllers/business.controllers'
 
 export const businessRouter = Router()
 
 businessRouter.post('/qr-scan', businessQRScanController)
 businessRouter.post('/award-points', businessAwardPointsController)
+businessRouter.post('/get-name-and-discount', businessGetNameAndDiscount)
