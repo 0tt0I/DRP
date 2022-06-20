eef29d0b77e3e710a8ffa0075321d0d51565c868
r-ushil <rushil.patel791@gmail.com>
2022-06-20 21:17:43 +0000

    style: removed unused imports

--- app/services/businessInfo.ts
+++ app/services/businessInfo.ts
@@ -1,13 +1,9 @@
-import { doc, getDoc } from '@firebase/firestore'
-import { db } from '../firebase'
-
 export async function getNameAndDiscount (businessUid: string) {
-  
   const res = await fetch('/api/business/get-name-and-discount', {
     method: 'POST',
-    body: JSON.stringify({businessUid}),
+    body: JSON.stringify({ businessUid }),
     headers: {
-      'Content-Type': 'application/json',
+      'Content-Type': 'application/json'
     }
   })
 
