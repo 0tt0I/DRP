57397375cb420b8c57e3e44af17b0abe4d108919
r-ushil <rushil.patel791@gmail.com>
2022-06-20 21:43:25 +0100

    feat: implemented customerGetPointsController

--- api/src/controllers/customer.controllers.ts
+++ api/src/controllers/customer.controllers.ts
@@ -1,5 +1,19 @@
+import { doc, getDoc } from '@firebase/firestore'
 import { Request, Response } from 'express'
+import { db } from 'src/plugins/firebase'
 
 export async function customerGetPointsController(req: Request, res: Response) {
+
+  const customerUid: string = req.body.customerUid
+  const businessUid: string = req.body.businessUid
     
+  const docRef = doc(db, 'customers', customerUid, 'businesses', businessUid)
+  const docSnap = await getDoc(docRef)
+
+  if (docSnap.exists()) {
+    const pointsEarned = docSnap.data().pointsEarned
+    res.status(200).json({ pointsEarned })
+  } else {
+    res.status(200).json({ pointsEarned: -1 })
+  }
 }
