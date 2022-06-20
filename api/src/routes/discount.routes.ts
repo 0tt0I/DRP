ed5b3291280617b9f41ccb61d86bcc3f77d51211
r-ushil <rushil.patel791@gmail.com>
2022-06-20 22:42:14 +0000

    feat: added route for get info controller

--- api/src/routes/discount.routes.ts
+++ api/src/routes/discount.routes.ts
@@ -1,3 +1,6 @@
 import { Router } from 'express'
+import { discountGetInfoController } from 'src/controllers/discount.controller'
 
 export const discountRouter = Router()
+
+discountRouter.post('/get-discount-info', discountGetInfoController)
