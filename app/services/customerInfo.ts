86407b7052a7e843e3e9fb648428d803051f54d5
r-ushil <rushil.patel791@gmail.com>
2022-06-20 13:45:34 +0100

    feat: extracted getPointsEarned function that takes in a customer and business UID

---
+++ app/services/customerInfo.ts
@@ -0,0 +1,15 @@
+import { doc, getDoc } from "@firebase/firestore"
+import { db } from "../firebase"
+
+export async function getPointsEarned (customerUid: string, businessUid: string): Promise<Number> {
+
+    const docRef = doc(db, 'customers', customerUid, 'businesses', businessUid)
+    const docSnap = await getDoc(docRef)
+
+    if (docSnap.exists()) {
+        return (docSnap.data().pointsEarned)
+    } else {
+        return -1 // document doesn't exist
+    }
+
+}
