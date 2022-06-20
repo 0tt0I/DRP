a139d458c960e565a40e820a71d42596364b3289
r-ushil <rushil.patel791@gmail.com>
2022-06-20 23:13:37 +0000

    feat: added add discount route

--- api/src/routes/discount.routes.ts
+++ api/src/routes/discount.routes.ts
@@ -1,8 +1,8 @@
 import { Router } from 'express'
-import { discountGetInfoController, discountsGetAllController } from '../controllers/discount.controllers'
+import { discountAddController, discountGetInfoController, discountsGetAllController } from '../controllers/discount.controllers'
 
 export const discountRouter = Router()
 
 discountRouter.post('/get-discount-info', discountGetInfoController)
 discountRouter.post('/get-all-discounts', discountsGetAllController)
-
+discountRouter.post('/add-discount', discountAddController)
