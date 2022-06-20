2191d6caa4498845f74c7a260b699d7fd238f434
r-ushil <rushil.patel791@gmail.com>
2022-06-20 21:32:57 +0000

    feat: wrote route for getOtherReferrals

--- api/src/routes/customer.routes.ts
+++ api/src/routes/customer.routes.ts
@@ -1,8 +1,14 @@
 import { Router } from 'express'
-import { customerGetPointsController, customerGetUserReferrals, customerUpdatePointsController } from '../controllers/customer.controllers'
+import {
+  customerGetOtherReferrals,
+  customerGetPointsController,
+  customerGetUserReferrals,
+  customerUpdatePointsController
+} from '../controllers/customer.controllers'
 
 export const customerRouter = Router()
 
 customerRouter.post('/get-points', customerGetPointsController)
 customerRouter.post('/update-points', customerUpdatePointsController)
 customerRouter.post('/get-user-referrals', customerGetUserReferrals)
+customerRouter.post('/get-other-referrals', customerGetOtherReferrals)
