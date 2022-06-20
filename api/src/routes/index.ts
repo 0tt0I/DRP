b7d4b36a6d6d1ab9ba714e77483bd55de51101e9
r-ushil <rushil.patel791@gmail.com>
2022-06-20 22:33:55 +0000

    feat: added discount router skeleton and correct middleware routing

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -2,8 +2,10 @@ import { Router } from 'express'
 
 import { businessRouter } from './business.routes'
 import { customerRouter } from './customer.routes'
+import { discountRouter } from './discount.routes'
 
 export const router = Router()
 
 router.use('/business', businessRouter)
 router.use('/customer', customerRouter)
+router.use('/discount', discountRouter)
