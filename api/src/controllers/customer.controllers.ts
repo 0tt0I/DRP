2cd7c571cbd3e8d2e630a56c6d8e60589d7981d2
r-ushil <rushil.patel791@gmail.com>
2022-06-20 21:17:10 +0000

    feat: added updatePointsController

--- api/src/controllers/customer.controllers.ts
+++ api/src/controllers/customer.controllers.ts
@@ -1,12 +1,11 @@
-import { doc, getDoc } from '@firebase/firestore'
+import { doc, getDoc, updateDoc } from '@firebase/firestore'
 import { Request, Response } from 'express'
 import { db } from '../plugins/firebase'
 
-export async function customerGetPointsController(req: Request, res: Response) {
-
+export async function customerGetPointsController (req: Request, res: Response) {
   const customerUid: string = req.body.customerUid
   const businessUid: string = req.body.businessUid
-    
+
   const docRef = doc(db, 'customers', customerUid, 'businesses', businessUid)
   const docSnap = await getDoc(docRef)
 
@@ -16,4 +15,17 @@ export async function customerGetPointsController(req: Request, res: Response) {
   } else {
     res.status(200).json({ pointsEarned: -1 })
   }
+}
+
+export async function customerUpdatePointsController (req: Request, res: Response) {
+  const customerUid: string = req.body.customerUid
+  const businessUid: string = req.body.businessUid
+  const newPoints: number = req.body.newPoints
+
+  const docRef = doc(db, 'customers', customerUid, 'businesses', businessUid)
+  await updateDoc(docRef, {
+    pointsEarned: newPoints
+  })
+
+  res.status(200)
 }
