8a0c895ba49f2a32d81a29358df6471041edc626
Abdullah, Fawwaz <crashmacompilers@gmail.com>
2022-06-20 15:48:24 +0100

    styles: added descriptions to the home pages

--- app/pages/business-home.tsx
+++ app/pages/business-home.tsx
@@ -16,33 +16,45 @@ export default function BusinessHome () {
     <div className="home-div">
       <div className="home-subdiv">
         <h1>
-          What would you like to do as a business?
+          Business Home
         </h1>
 
-        <div className="home-buttondiv">
+        <div className="home-bus-buttondiv">
           <button onClick={logout} className="general-button">
             Log Out
           </button>
+
+          <p>Log out of your account.</p>
 
           <button onClick={() => router.push('/business-referral-scanner')} className="general-button">
             Scan Discount Code
           </button>
 
+          <p>Scan and verify a discount code for your business.</p>
+
           <button onClick={() => router.push('/edit-discounts')} className="general-button">
             Edit Discounts
           </button>
+
+          <p>Edit the available discount offers at your business.</p>
 
           <button onClick={() => router.push('/business-qr-code')} className="general-button">
             Your QR Code
           </button>
 
+          <p>Fetch the unique location-identifying QR code for your businesss.</p>
+
           <button onClick={() => router.push('/business-point-checker')} className="general-button">
             Check Points
           </button>
 
+          <p>Scan a customer&apos;s referral QR code to check their progress for another discount.</p>
+
           <button onClick={() => router.push('/business-reward-claim')} className="general-button">
             Reward
           </button>
+
+          <p>Scan and use up a customer&apos;s returning discount code.</p>
         </div>
       </div>
     </div>
