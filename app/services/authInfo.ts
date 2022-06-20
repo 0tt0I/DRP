2bf2c264436d646b0de3383ab912c7176dfea484
r-ushil <rushil.patel791@gmail.com>
2022-06-20 16:10:02 +0000

    style: fixed linter errors

--- app/services/authInfo.ts
+++ app/services/authInfo.ts
@@ -1,9 +1,9 @@
-import { auth } from "../firebase"
+import { auth } from '../firebase'
 
 export function getUid (): string {
-    return auth.currentUser!.uid
+  return auth.currentUser!.uid
 }
 
 export function getUserEmail (): string {
-    return auth.currentUser!.email!
+  return auth.currentUser!.email!
 }
