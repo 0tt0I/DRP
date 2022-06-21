c20fc5f9e21784087bec3e4f13def33723742e00
0tt0I <ow20@ic.ac.uk>
2022-06-21 06:49:30 +0100

    feat: initialize firebase admin sdk

---
+++ api/src/plugins/firebase-admin.ts
@@ -0,0 +1,7 @@
+import * as admin from 'firebase-admin'
+import { join } from 'path'
+import { environmentConfig } from '../config/config'
+
+export const firebaseAdmin = admin.initializeApp({
+  credential: admin.credential.cert(join(environmentConfig.apiRoot, '/firebase-admin-sdk.json'))
+})
