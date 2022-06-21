f2443ac9c6ada839e5a38bc1593f302a53d4a583
r-ushil <rushil.patel791@gmail.com>
2022-06-21 13:19:55 +0000

    feat: made use of VisitedBusiness and Discount in redeem-reward

--- app/pages/customer/redeem-reward.tsx
+++ app/pages/customer/redeem-reward.tsx
@@ -5,16 +5,9 @@ import QRUid from '../../components/QRUid'
 import { Dialog } from '@headlessui/react'
 import { createHash } from 'crypto'
 import { getUid } from '../../services/authInfo'
-import { Businesses } from '../../types/FirestoreCollections'
+import { Discount, VisitedBusiness } from '../../types/FirestoreCollections'
 import { getVisitedBusinesses } from '../../services/customerInfo'
-
-interface ReedemableDiscount {
-  pointsEarned: number,
-  pointsNeeded: number,
-  description: string,
-  discountUid: string,
-  place: string
-}
+import { getEligibleDiscounts } from '../../services/discountInfo'
 
 export default function RedeemReward () {
   const router = useRouter()
@@ -26,10 +19,10 @@ export default function RedeemReward () {
   // modal state for popup and info for displaying discounts
   const [discountOpen, setDiscountOpen] = useState(false)
 
-  // eslint-disable-next-line no-unused-vars
-  const [discounts, setDiscounts] = useState<ReedemableDiscount[]>([])
-  const [businesses, setBusinesses] = useState<Businesses[]>([])
+  const [discounts, setDiscounts] = useState<Discount[]>([])
+  const [businesses, setBusinesses] = useState<VisitedBusiness[]>([])
   const [initialLoad, setInitialLoad] = useState(true)
+  const [selectedBusiness, setSelectedBusiness] = useState<VisitedBusiness>()
 
   useEffect(() => {
     const getBusinesses = async () => {
@@ -43,14 +36,16 @@ export default function RedeemReward () {
     }
   })
 
-  // useEffect(() => {
-  //   const relevantDiscounts = async () => {
-  //     const jsonResponse = await getBusinessDisconts(businessUid)
-  //     setDiscounts(jsonResponse.discounts)
-  //   }
+  useEffect(() => {
+    const relevantDiscounts = async () => {
+      if (selectedBusiness) {
+        const jsonResponse = await getEligibleDiscounts(selectedBusiness.id!, selectedBusiness.pointsEarned)
+        setDiscounts(jsonResponse.discounts)
+      }
+    }
 
-  //   relevantDiscounts()
-  // }, [discountOpen])
+    relevantDiscounts()
+  }, [discountOpen])
 
   return (
     <div className="home-div">
@@ -68,7 +63,7 @@ export default function RedeemReward () {
     </div>
   )
 
-  function BusinessEntry (business: Businesses) {
+  function BusinessEntry (business: VisitedBusiness) {
     return (
       <div
         key={createHash('sha256').update(JSON.stringify(business)).digest('hex').toString()}
@@ -79,7 +74,10 @@ export default function RedeemReward () {
             <h1 className="font-bold text-dark-nonblack">Place</h1>
             <p className="col-span-2">{business.name}</p>
 
-            <button className="general-button" onClick={() => { setDiscountOpen(true) }}>
+            <button className="general-button" onClick={() => {
+              setSelectedBusiness(business)
+              setDiscountOpen(true)
+            }}>
             Redeem
             </button>
 
@@ -115,55 +113,50 @@ export default function RedeemReward () {
     )
   }
 
-  function DiscountEntry (discount: ReedemableDiscount) {
+  function DiscountEntry (discount: Discount) {
     return (
       <div
         key={createHash('sha256').update(JSON.stringify(discount)).digest('hex').toString()}
         className="flex flex-col gap-4 place-content-center p-4 default-div rounded-lg min-w-max">
         <div className="flex flex-row gap-4 place-content-start">
 
-          <div className="ref-info grid grid-cols-3 grid-flow-row-dense place-content-center gap-2 min-w-max">
-            <h1 className="font-bold text-dark-nonblack">Place</h1>
-            <p className="col-span-2">{discount.place}</p>
+          <h1 className="font-bold text-dark-nonblack">Description</h1>
+          <p className="col-span-2 break-all w-80">{discount.description}</p>
 
-            <h1 className="font-bold text-dark-nonblack">Description</h1>
-            <p className="col-span-2 break-all w-80">{discount.description}</p>
+          <h1 className="font-bold text-dark-nonblack w-32">Points Needed</h1>
+          <p className="col-span-2">{discount.points}</p>
 
-            <h1 className="font-bold text-dark-nonblack w-32">Points Needed</h1>
-            <p className="col-span-2">{discount.pointsNeeded}</p>
+          <h1 className="font-bold text-dark-nonblack w-32">Points Earnt</h1>
+          <p className="col-span-2">{selectedBusiness?.pointsEarned}</p>
 
-            <h1 className="font-bold text-dark-nonblack w-32">Points Earnt</h1>
-            <p className="col-span-2">{discount.pointsEarned}</p>
-
-          </div>
-
-          <button className="general-button" onClick={() => setQrOpen(true)}>
-            Redeem
-          </button>
-
-          <Dialog open={qrOpen} onClose={() => setQrOpen(false)} className="relative z-50">
-            <div className="fixed inset-0 flex items-center justify-center p-4 drop-shadow-lg">
-              <Dialog.Panel className="w-full max-w-md overflow-hidden p-4 text-left align-middle shadow-xl transition-all flex flex-col gap-4 ultralight-div">
-                <Dialog.Title as="h3" className="font-bold text-center text-4xl text-dark-nonblack">
-              Your QR Code
-                </Dialog.Title>
-                <Dialog.Description>
-                  <div className="flex flex-col grow text-center">
-                    <p>Ask to scan the QR code at the till to claim!:</p>
-                  </div>
-                </Dialog.Description>
-
-                <div className="place-self-center">
-                  <QRUid uid={uid.current + '-' + discount.discountUid}/>
-                </div>
-
-                <button className="general-button" onClick={() => setQrOpen(false)}>
-                Cancel
-                </button>
-              </Dialog.Panel>
-            </div>
-          </Dialog>
         </div>
+
+        <button className="general-button" onClick={() => setQrOpen(true)}>
+            Redeem
+        </button>
+
+        <Dialog open={qrOpen} onClose={() => setQrOpen(false)} className="relative z-50">
+          <div className="fixed inset-0 flex items-center justify-center p-4 drop-shadow-lg">
+            <Dialog.Panel className="w-full max-w-md overflow-hidden p-4 text-left align-middle shadow-xl transition-all flex flex-col gap-4 ultralight-div">
+              <Dialog.Title as="h3" className="font-bold text-center text-4xl text-dark-nonblack">
+              Your QR Code
+              </Dialog.Title>
+              <Dialog.Description>
+                <div className="flex flex-col grow text-center">
+                  <p>Ask to scan the QR code at the till to claim!:</p>
+                </div>
+              </Dialog.Description>
+
+              <div className="place-self-center">
+                <QRUid uid={uid.current + '-' + discount.id}/>
+              </div>
+
+              <button className="general-button" onClick={() => setQrOpen(false)}>
+                Cancel
+              </button>
+            </Dialog.Panel>
+          </div>
+        </Dialog>
       </div>
     )
   }
