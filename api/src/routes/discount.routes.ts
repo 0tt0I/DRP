b67320c1bf34649efaae0dd3b9eaddc7bfdf7926
r-ushil <rushil.patel791@gmail.com>
2022-06-21 13:19:18 +0000

    feat: added correct route for get-eligible-discounts

--- api/src/routes/discount.routes.ts
+++ api/src/routes/discount.routes.ts
@@ -1,8 +1,13 @@
 import { Router } from 'express'
-import { discountAddController, discountGetInfoController, discountsGetAllController } from '../controllers/discount.controllers'
+import {
+  discountAddController,
+  discountGetEligibleController,
+  discountGetInfoController, discountsGetAllController
+} from '../controllers/discount.controllers'
 
 export const discountRouter = Router()
 
 discountRouter.post('/get-discount-info', discountGetInfoController)
 discountRouter.post('/get-all-discounts', discountsGetAllController)
 discountRouter.post('/add-discount', discountAddController)
+discountRouter.post('/get-eligible-discounts', discountGetEligibleController)
