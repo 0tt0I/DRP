cf0ff975885ff75a1ca4d60610b275379ea6fa7c
Edward Hartley <edward.hartley20@imperial.ac.uk>
2022-06-22 02:13:02 +0000

    feat: restructure customer UI frontend

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -18,35 +18,23 @@ export default function Home () {
         </h1>
 
         <div className="home-buttondiv">
+          <button onClick={() => router.push('/customer/referrals')} className="general-button">
+            Discover
+          </button>
+
+          <p>Find a referral you like and discover a new place.</p>
+
+          <button onClick={() => router.push('/customer/promoter-home')} className="general-button">
+            Spread the Word
+          </button>
+
+          <p>Create your own referrals to earn loyalty points and then redeem those points.</p>
+
           <button onClick={logout} className="general-button">
             Log Out
           </button>
 
           <p>Log out of your account.</p>
-
-          <button onClick={() => router.push('/customer/referrals')} className="general-button">
-            Redeem Referrals
-          </button>
-
-          <p>Use someone&apos;s referral for a discount.</p>
-
-          <button onClick={() => router.push('/customer/add-referral')} className="general-button">
-            New Referral
-          </button>
-
-          <p>Create a new referral for a location.</p>
-
-          <button onClick={() => router.push('/customer/my-referrals')} className="general-button">
-            My Referrals
-          </button>
-
-          <p>See your created referrals and their next discount progress.</p>
-
-          <button onClick={() => router.push('/customer/redeem-reward')} className="general-button">
-            Redeem Rewards
-          </button>
-
-          <p>Redeem your available discounts for a location.</p>
         </div>
       </div>
     </div>
