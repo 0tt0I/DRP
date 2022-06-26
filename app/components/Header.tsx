3a7c08a4ff70050b1bb404015a1db736ee5be630
Abdullah, Fawwaz <crashmacompilers@gmail.com>
2022-06-26 22:00:37 +0100

    feat: added more back buttons on the promoter's side

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,17 +1,16 @@
-import { NextRouter } from 'next/router'
 import React from 'react'
 import BackButton from './BackButton'
 
 export type HeaderProps = {
-  router: NextRouter
   text: string
   where?: string
+  onClick?: React.MouseEventHandler<HTMLButtonElement>
 }
 
 export default function Header (props: HeaderProps) {
   return (
     <div className="flex flex-row sm:relative sm:gap-0 gap-2 p-0.5 home-header items-center justify-center min-w-max">
-      <BackButton className="sm:absolute z-10 left-0" router={props.router} where={props.where} />
+      <BackButton onClick={props.onClick} className="sm:absolute z-10 left-0" where={props.where} />
       <h1 className="text-center grow">{props.text}</h1>
     </div>
   )
