830ba79aa32b7f9d39a07cf8caa3debf51a9877a
Abdullah, Fawwaz <crashmacompilers@gmail.com>
2022-06-26 21:49:38 +0100

    feat: added back button to all promoter pages

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -10,8 +10,8 @@ export type HeaderProps = {
 
 export default function Header (props: HeaderProps) {
   return (
-    <div className="flex flex-row gap-0 p-0.5 home-header items-center justify-center">
-      <BackButton router={props.router} where={props.where} />
+    <div className="flex flex-row sm:relative sm:gap-0 gap-2 p-0.5 home-header items-center justify-center min-w-max">
+      <BackButton className="sm:absolute z-10 left-0" router={props.router} where={props.where} />
       <h1 className="text-center grow">{props.text}</h1>
     </div>
   )
