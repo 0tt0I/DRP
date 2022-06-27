9b2149155a22391a39f17be5133df66a68f9fe61
Abdullah, Fawwaz <crashmacompilers@gmail.com>
2022-06-27 11:20:00 +0100

    feat: display loader on top of everything else

--- app/components/LoadingPlaceholder.tsx
+++ app/components/LoadingPlaceholder.tsx
@@ -4,7 +4,7 @@ const commonClasses = 'absolute h-4 w-4 bg-white rounded-full '
 
 export default function LoadingPlaceholder () {
   return (
-    <div className="home-div loader-div">
+    <div className="loader-div">
       <div className='relative w-12 h-12 animate-spin'>
         <div className={commonClasses + 'opacity-40 top-0 left-0'}/>
         <div className={commonClasses + 'opacity-60 top-0 right-0'}/>
