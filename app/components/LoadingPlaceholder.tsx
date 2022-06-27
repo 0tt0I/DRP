9a60d65b12b119df8b1d2763303ace81e436444f
Abdullah, Fawwaz <crashmacompilers@gmail.com>
2022-06-27 11:05:06 +0100

    feat: added loading spinner

---
+++ app/components/LoadingPlaceholder.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const commonClasses = 'absolute h-4 w-4 bg-white rounded-full '
+
+export default function LoadingPlaceholder () {
+  return (
+    <div className="home-div loader-div">
+      <div className='relative w-12 h-12 animate-spin'>
+        <div className={commonClasses + 'opacity-40 top-0 left-0'}/>
+        <div className={commonClasses + 'opacity-60 top-0 right-0'}/>
+        <div className={commonClasses + 'opacity-80 bottom-0 right-0'}/>
+        <div className={commonClasses + 'opacity-100 bottom-0 left-0'}/>
+      </div>
+    </div>
+  )
+}
--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -2,12 +2,13 @@ import '../styles/styles.css'
 import React from 'react'
 import { AuthProvider } from '../hooks/useAuth'
 import { AppProps } from 'next/app'
+import LoadingPlaceholder from '../components/LoadingPlaceholder'
 
 function MyApp ({ Component, pageProps }: AppProps) {
   // wrap entire application in authentication
-
   return (
     <AuthProvider>
+      <LoadingPlaceholder />
       <Component {...pageProps} />
     </AuthProvider>
   )
