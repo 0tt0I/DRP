1804b2c971b879be42061f8f148f742b67fbf5a3
Fawwaz Abdullah <crashmacompilers@gmail.com>
2022-06-28 16:45:03 +0100

    fix: finally squashed that refresh bug by adding proper routers for all pages

---
+++ api/src/static.routes/customer.static.routes.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express'
+import { join } from 'path'
+import { environmentConfig } from '../config/config'
+
+export const customerRouter = Router()
+
+customerRouter.get('*', (req, res) => {
+  res.sendFile(join(environmentConfig.appCompiledStatic, req.path + '.html'))
+})
