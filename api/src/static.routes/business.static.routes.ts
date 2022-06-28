8c7fc5a3b81e8564480c4d4dc820cc3d28bda679
Fawwaz Abdullah <crashmacompilers@gmail.com>
2022-06-28 18:17:21 +0100

    fix: just manually resolve the paths on the top level...

--- api/src/static.routes/business.static.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'
-import { join } from 'path'
-import { environmentConfig } from '../config/config'
-
-export const businessRouter = Router()
-
-businessRouter.use('/', (req, res) => {
-  res.sendFile(join(environmentConfig.appCompiledStatic, req.path + '.html'))
-})
