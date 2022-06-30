bde88b105ff250a2ab9ebc3bc977461b464b4512
Abdullah, Fawwaz <crashmacompilers@gmail.com>
2022-06-30 20:23:12 +0100

    fix: scrolling fixed on small screens, shortened management header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -11,7 +11,7 @@ export default function Header (props: HeaderProps) {
   return (
     <div className="flex flex-row sm:relative sm:gap-0 gap-2 p-0.5 home-header items-center justify-center min-w-max">
       <BackButton onClick={props.onClick} className="sm:absolute z-10 left-0" where={props.where} />
-      <h1 className="text-center grow">{props.text}</h1>
+      <h1 className="text-center grow break-words">{props.text}</h1>
     </div>
   )
 }
--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -8,7 +8,7 @@ function MyApp ({ Component, pageProps }: AppProps) {
   // wrap entire application in authentication
   return (
     <AuthProvider>
-      <div className="max-w-full min-h-screen flex flex-col items-center justify-center gap-8 p-8">
+      <div className="max-w-full min-h-max flex flex-col items-center justify-center gap-8 p-8">
         <img src="/ml.png" />
         <div id="loading-and-content">
           <LoadingPlaceholder />
