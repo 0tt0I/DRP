b8249968af13383f0127503cd33fd7f904c473ab
Abdullah, Fawwaz <crashmacompilers@gmail.com>
2022-07-01 08:44:19 +0100

    fix: last-minute layout fixes

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -8,7 +8,7 @@ function MyApp ({ Component, pageProps }: AppProps) {
   // wrap entire application in authentication
   return (
     <AuthProvider>
-      <div className="max-w-full h-max min-h-screen flex flex-col items-center justify-center gap-8 p-8">
+      <div className="resp-scale min-w-screen min-h-screen flex flex-col items-center justify-center gap-8">
         <img src="/ml.png" />
         <div id="loading-and-content">
           <LoadingPlaceholder />
